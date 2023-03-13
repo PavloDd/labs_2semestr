bea195bbf62e0e8b7f20ef980a403eb78eddfabe
PavloDd <86347147+PavloDd@users.noreply.github.com>
2023-03-13 13:21:08 +0200

    changed code by review

--- src/ua/lviv/iot/algo/part1/lab1/Fridge.java
+++ src/ua/lviv/iot/algo/part1/lab1/Fridge.java
@@ -6,46 +6,37 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 @AllArgsConstructor
-
 public class Fridge {
     private String brand;
     private String model;
     private String capacity;
     private boolean isDefrosing;
     private char energyEfficiencyClass;
-    private static Fridge instance;
-
+    private static ua.lviv.iot.algo.part1.lab1.Fridge instance;
     public void turnOnDefrosing(){
         this.isDefrosing = true;
-        System.out.println(getModel()+" is defrosing");
     }
 
     public void turnOffDefrosing(){
         this.isDefrosing = false;
-        System.out.println(getModel()+" isn`t defrosing");
     }
 
     public void deleteModelInfo(){
         this.model = null;
     }
-    
-    public static Fridge getInstance(){
+
+    public static ua.lviv.iot.algo.part1.lab1.Fridge getInstance(){
         if (instance == null){
-            instance = new Fridge();
+            instance = new ua.lviv.iot.algo.part1.lab1.Fridge();
         }
         return instance;
     }
 
     public static void main(String[] args) {
-        Fridge fridges[] = new Fridge[4];
-        fridges[0] = new Fridge("Bosch","in132","20 l",true,'B');
-        fridges[1] = new Fridge();
-        fridges[2] = Fridge.getInstance();
-        fridges[3] = Fridge.getInstance();
-        int i = 0;
-        while ( i < fridges.length) {
-            System.out.println(fridges[i].toString());
-            i += 1;
+
+        ua.lviv.iot.algo.part1.lab1.Fridge fridges[] = {new ua.lviv.iot.algo.part1.lab1.Fridge("Bosch","in132","20 l",true,'B'), new ua.lviv.iot.algo.part1.lab1.Fridge(), ua.lviv.iot.algo.part1.lab1.Fridge.getInstance(), ua.lviv.iot.algo.part1.lab1.Fridge.getInstance()};
+        for (ua.lviv.iot.algo.part1.lab1.Fridge value : fridges){
+            System.out.println(value.toString());
         }
     }
 }
